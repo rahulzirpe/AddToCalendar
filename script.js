d900669cae3d3b37899eeb59bd7461f64afbe0f8
rahulzirpe <123835457+rahulzirpe@users.noreply.github.com>
2024-10-24 17:47:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ if (preferredDateInput) {
 	if (numQuickReplies == "1") {
             var cmdName = lpTag.agentSDK.cmdNames.writeSC;
             var quickReply1 = document.getElementById('quickReply1').value;
-            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+            var text1=`Please select your preferred time slot for ${formattedDate}`
 
             var data = {
                 json:{
@@ -148,7 +148,7 @@ if (preferredDateInput) {
             var cmdName = lpTag.agentSDK.cmdNames.writeSC;
             var quickReply1 = document.getElementById('quickReply1').value;
             var quickReply2 = document.getElementById('quickReply2').value;
-            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+            var text1=`Please select your preferred time slot for ${formattedDate}`
 
             var data = {
                 json:{
@@ -195,7 +195,7 @@ if (preferredDateInput) {
             var quickReply2 = document.getElementById('quickReply2').value;
             var quickReply3 = document.getElementById('quickReply3').value;
 
-            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+            var text1=`Please select your preferred time slot for ${formattedDate}`
 
             var data = {
                 json:{
@@ -255,7 +255,7 @@ if (preferredDateInput) {
             var quickReply3 = document.getElementById('quickReply3').value;
             var quickReply4 = document.getElementById('quickReply4').value;
 
-            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+            var text1=`Please select your preferred time slot for ${formattedDate}`
 
             var data = {
                 json:{
@@ -330,7 +330,7 @@ if (preferredDateInput) {
             var quickReply4 = document.getElementById('quickReply4').value;
             var quickReply5 = document.getElementById('quickReply5').value;
 
-            var text1=`Please select Preferrable Time Slot for ${formattedDate}`
+            var text1=`Please select your preferred time slot for ${formattedDate}`
 
             var data = {
                 json:{
